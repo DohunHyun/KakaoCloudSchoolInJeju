c59f4230f2ed515ab214ff701ec4e56a2028340b
DohunHyun <dohun.hyun@gmail.com>
2024-04-19 21:37:30 +0900

    json fetch 하기

--- project/week2_2/js/board.js
+++ project/week2_2/js/board.js
@@ -16,3 +16,38 @@ writeBtn.addEventListener('click', function() {
 post1.addEventListener('click', function() {
     location.href='/html/postDetail.html';
 })
+
+function loadPostList() {
+    return fetch("/json/postList.json")
+        .then( (res) => res.json())
+        .then( (json) => json.items);
+}
+
+loadPostList().then( (items) => {
+    console.log(items);
+    displayPostList(items);
+})
+
+function displayPostList(items) {
+    const container = document.getElementById('post-list');
+    container.innerHTML = items.map((item) => createPostListHTML(item)).join("");
+}
+
+function createPostListHTML(item) {
+    return `
+        <article class="post-box">
+            <div class="post-detail">
+                <div class="post-title">${item.title}</div>
+                <div class="post-info">
+                    <p class="post-reaction">${item.reaction}</p>
+                    <p class="post-time">${item.time}</p>
+                </div>
+            </div>
+            <hr class="post-hr">
+            <div class="post-writer">
+                <img src="${item.writerPic}">
+                <p class="writer-name">${item.writerName}</p>
+            </div>
+        </article>
+    `;
+}
